ea84b9d31800252f565ae2ef86ffb64aecfafa24
codephillip <codephillip@gmail.com>
2020-03-27 18:47:17 +0300

    added settings to collect staticfiles

--- SafepalDjangoBackend/settings.py
+++ SafepalDjangoBackend/settings.py
@@ -151,5 +151,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
