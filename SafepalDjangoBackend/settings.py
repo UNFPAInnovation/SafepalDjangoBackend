e9ae9a6804612d02a242c59b3c5bdf9eb7f4a817
codephillip <codephillip@gmail.com>
2020-03-28 13:58:19 +0300

    added settings to stop serialization of Decimals as Strings

--- SafepalDjangoBackend/settings.py
+++ SafepalDjangoBackend/settings.py
@@ -96,6 +96,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100,
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+    'COERCE_DECIMAL_TO_STRING': False
 }
 
 WSGI_APPLICATION = 'SafepalDjangoBackend.wsgi.application'
