af44eae6c2f869a35fe230b228263dc87ed309f7
codephillip <codephillip@gmail.com>
2020-05-27 14:50:33 +0300

    removed default menu items from wagtail dashboard, updated requirements

--- app/wagtail_hooks.py
+++ app/wagtail_hooks.py
@@ -1,4 +1,5 @@
 from wagtail.contrib.modeladmin.options import modeladmin_register, ModelAdmin, ModelAdminGroup
+from wagtail.core import hooks
 
 from .models import *
 
@@ -7,7 +8,7 @@ class ArticleAdmin(ModelAdmin):
     model = Article
     menu_label = "Articles"
     menu_icon = "doc-full"
-    menu_order = 000
+    menu_order = 100
     add_to_settings_menu = False
     exclude_from_explorer = False
     list_display = ("title", "thumbnail", "created_at")
@@ -19,7 +20,7 @@ class VideoAdmin(ModelAdmin):
     model = Video
     menu_label = "Video"
     menu_icon = "media"
-    menu_order = 100
+    menu_order = 200
     add_to_settings_menu = False
     exclude_from_explorer = False
     list_display = ("title", "url", "created_at")
@@ -54,7 +55,7 @@ class QuestionAdmin(ModelAdmin):
 class QuizGroup(ModelAdminGroup):
     menu_label = "QuizGroup"
     menu_icon = "list-ol"
-    menu_order = 200
+    menu_order = 300
     items = (QuizAdmin, QuestionAdmin)
 
 
@@ -62,7 +63,7 @@ class OrganizationAdmin(ModelAdmin):
     model = Organization
     menu_label = "Organizations"
     menu_icon = "group"
-    menu_order = 300
+    menu_order = 400
     add_to_settings_menu = False
     exclude_from_explorer = False
     list_display = ("facility_name", "district", "phone_number", "created_at")
@@ -74,7 +75,7 @@ class CategoryAdmin(ModelAdmin):
     model = Category
     menu_label = "Categories"
     menu_icon = "pick"
-    menu_order = 400
+    menu_order = 500
     add_to_settings_menu = False
     exclude_from_explorer = False
     list_display = ("name",)
@@ -85,7 +86,7 @@ class DistrictAdmin(ModelAdmin):
     model = District
     menu_label = "Districts"
     menu_icon = "site"
-    menu_order = 500
+    menu_order = 600
     add_to_settings_menu = False
     exclude_from_explorer = False
     list_display = ("name",)
@@ -98,3 +99,8 @@ modeladmin_register(QuizGroup)
 modeladmin_register(OrganizationAdmin)
 modeladmin_register(CategoryAdmin)
 modeladmin_register(DistrictAdmin)
+
+
+@hooks.register('construct_main_menu')
+def hide_default_explorer_menu(request, menu_items):
+    menu_items[:] = [item for item in menu_items if item.name not in ['settings', 'images', 'documents', 'reports', 'explorer']]
